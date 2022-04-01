568f36901694264be32061407c12785f2c5e559b
nvpetrov <nvpetrov@phoenixit.ru>
2022-04-01 20:11:14 +0300

    minor fixes and new webhook

--- src/main/java/com/example/demobot/SubscribeBot.java
+++ src/main/java/com/example/demobot/SubscribeBot.java
@@ -1,14 +1,9 @@
 package com.example.demobot;
 
 import com.example.demobot.config.BotFacade;
-import com.example.demobot.service.GetChatMemberUseCase;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -17,11 +12,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Getter
 @Setter
 @Slf4j
-
 public class SubscribeBot extends TelegramWebhookBot {
-    private String botToken = "5278510472:AAGfDAPhkInDdKUra0e4CkpV2hnK40R_4Kk";
-    private String botName = "sogr_bot";
-    private String webhookPath = "https://25ba-188-32-209-237.ngrok.io";
+
+
+    private String webhookPath;
+    private String botToken;
+    private String botName;
 
     private final BotFacade bot;
 
@@ -33,9 +29,7 @@ public class SubscribeBot extends TelegramWebhookBot {
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
         if (update.getMessage() != null && update.getMessage().hasText()) {
-
             return  bot.handleMessage(update);
-
         }
             return new SendMessage();
     }
